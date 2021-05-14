b3ae2587c8baab8a1663778dcfba027b5ea174a6
ashihs-giri-rampwin <ashish.giri@rampwin.com>
2021-05-14 13:27:12 +0530

    first commit

---
+++ classes_dict.py
@@ -0,0 +1,27 @@
+
+classes_dict = {}
+
+classes_dict["greeting"] = {}
+classes_dict["greeting"]["pattern"] = ["hi", "hi there", "hello", "hey", "good afternoon", "good morning", "good evening", "good day"]
+classes_dict["greeting"]["response"] = ["hi", "hey", "hello"]
+
+classes_dict["goodbye"] = {}
+classes_dict["goodbye"]["pattern"] = ["bye", "goodbye", "see you later", "gotta go", "i have to go", "see you", "see ya", "talk to you later"]
+classes_dict["goodbye"]["response"] = ["bye", "talk to you later"]
+
+classes_dict["thanks"] = {}
+classes_dict["thanks"]["pattern"] = ["thanks", "thank you"]
+classes_dict["thanks"]["response"] = ["you're welcome", "my pleasure", "don't mention it"]
+
+classes_dict["how are you"] = {}
+classes_dict["how are you"]["pattern"] = ["how are you", "how are you doing", "how's it going"]
+classes_dict["how are you"]["response"] = ["i'm doing ok", "ok", "i've been better"]
+
+classes_dict["future"] = {}
+classes_dict["future"]["pattern"] = ["will you", "can you", "would you", "do you"]
+classes_dict["future"]["response"] = ["yes", "no", "maybe"]
+
+classes_dict["when"] = {}
+classes_dict["when"]["pattern"] = ["when will", "when can", "when would", "when should"]
+classes_dict["when"]["response"] = ["soon", "not now"]
+
